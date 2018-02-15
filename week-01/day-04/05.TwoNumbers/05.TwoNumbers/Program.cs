92cdc8c7181869bbba4f2573240854ea50ac85f5
moggyancs <gcsuzdi@gmail.com>
2018-02-15 12:22:50 +0100

    task 05

---
+++ week-01/day-04/05.TwoNumbers/05.TwoNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05.TwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that prints a few operations on two numbers: 22 and 13
+            int numOne = 22;
+            int numTwo = 13;
+            // Print the result of 13 added to 22
+            Console.WriteLine(numOne + numTwo);
+            // Print the result of 13 substracted from 22
+            Console.WriteLine(numOne - numTwo);
+            // Print the result of 22 multiplied by 13
+            Console.WriteLine(numOne * numTwo);
+            // Print the result of 22 divided by 13 (as a decimal fraction)
+            object result = (Decimal)numOne / (Decimal)numTwo;
+            Console.WriteLine(result);
+            // Print the integer part of 22 divided by 13
+            Console.WriteLine(numOne / numTwo);
+            // Print the reminder of 22 divided by 13
+            Console.WriteLine(numOne % numTwo);
+            Console.ReadLine();
+        }
+    }
+}
