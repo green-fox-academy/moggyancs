eb501a81ef742343a444f0788f14c56fe5457023
moggyancs <gcsuzdi@gmail.com>
2018-02-15 14:01:06 +0100

    task 07

---
+++ week-01/day-04/07.FavoriteNumber/07.FavoriteNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _07.FavoriteNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Store your favorite number in a variable (as a number)
+            // And print it like this: "My favorite number is: 8"
+
+            Console.WriteLine("What is your favourite number? ");
+            decimal favNr = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("So your favourite number is " + favNr);
+            Console.ReadLine();
+        }
+    }
+}
