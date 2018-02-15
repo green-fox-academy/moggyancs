08239a9ddf79c5389cd8b39eaa154e77bf0c64c4
moggyancs <gcsuzdi@gmail.com>
2018-02-15 14:10:50 +0100

    task 08

---
+++ week-01/day-04/08.Swap/08.Swap/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08.Swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Swap the values of the variables
+            int a = 123;
+            int b = 526;
+
+            Console.WriteLine("Value of a and b before sawapping");
+            Console.WriteLine();
+            Console.WriteLine("a=" + " " + a);
+            Console.WriteLine("b=" + " " + b);
+
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine();
+            Console.WriteLine("Value of a and b after sawapping");
+            Console.WriteLine();
+            Console.WriteLine("a=" + " " + a);
+            Console.WriteLine("b=" + " " + b);
+            Console.ReadLine();
+        }
+    }
+}
