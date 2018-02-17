67edf2851b42bf543a16b6aab6b6bc3dec12f99d
moggyancs <gcsuzdi@gmail.com>
2018-02-17 16:57:31 +0100

    taks 27-28

---
+++ week-01/day-05/28.DrawTriangle/28.DrawTriangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _28.DrawTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Write a program that reads a number from the standard input, then draws a
+            // triangle like this:
+            //
+            // *
+            // **
+            // ***
+            // ****
+            //
+            // The triangle should have as many lines as the number was
+
+            Console.WriteLine("How tall is my pyramid?");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {                
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");                    
+                }     
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
