1db4d60288a10bde0cb105c30d737fd3aab6bde8
moggyancs <gcsuzdi@gmail.com>
2018-02-17 17:37:24 +0100

    task 29

---
+++ week-01/day-05/29.DrawPyramid/29.DrawPyramid/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _29.DrawPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // pyramid like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //
+            // The pyramid should have as many lines as the number was
+            Console.WriteLine("How tall is my pyramid?");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int k = height - i; k > 1; k--)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+
+                for (int j = 1; j <= i; j++ )
+                    {
+                        Console.Write("**");
+                    }                
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
