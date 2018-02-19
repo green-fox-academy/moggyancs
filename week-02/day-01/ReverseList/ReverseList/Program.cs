501da42c6b237826e256f6a375f100e9463b6075
moggyancs <gcsuzdi@gmail.com>
2018-02-19 17:19:56 +0100

    Arrays 13

---
+++ week-02/day-01/ReverseList/ReverseList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReverseList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `aj`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Reverse the order of the elements in `aj`
+            // - Print the elements of the reversed `aj`
+            int[] aj = new int[] { 3, 4, 5, 6, 7 };
+            Array.Reverse(aj);
+            foreach (var item in aj)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
