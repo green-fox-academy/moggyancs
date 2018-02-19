fe577a87b12a56021f9a399ca7fe28eb62c3d7fb
moggyancs <gcsuzdi@gmail.com>
2018-02-19 17:06:06 +0100

    arrays 11-12

---
+++ week-02/day-01/SumAllArray/SumAllArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SumAllArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `ai`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Print the sum of the elements in `ai`
+
+            int[] ai = new int[] { 3, 4, 5, 6, 7 };
+            int sum = 0;
+            for (int i = 0; i < ai.Length; i++)
+            {
+                sum += ai[i];
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-02/day-01/SwapElements/SwapElements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SwapElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // - Create an array variable named `abc`
+            //   with the following content: `["first", "second", "third"]`
+            // - Swap the first and the third element of `abc`
+
+            string[] abc = new string[] { "first", "second", "third" };
+            string temp = abc[0];
+            abc[0] = abc[2];
+            abc[2] = temp;
+            foreach (var item in abc)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
