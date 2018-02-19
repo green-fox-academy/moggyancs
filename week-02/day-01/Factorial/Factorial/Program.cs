4dae912cbe952f25fedd48d9c71d766e7013f562
moggyancs <gcsuzdi@gmail.com>
2018-02-19 14:50:42 +0100

    Functions 5-6

---
+++ week-02/day-01/Factorial/Factorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a function called `factorio`
+            //   that returns it's input's factorial
+            Console.WriteLine("I can calculate factorial. Give me a number!");
+            int myNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorio(myNumber));
+
+            Console.ReadLine();
+        }
+        static int Factorio(int number)
+        {
+            int facto = 1;
+            for (int i = number; i>=1; i--)
+            {
+                facto *= i;
+            }
+            return facto;
+        }
+            
+		
+    }
+}
---
+++ week-02/day-01/Printer/Printer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a function called `printer`
+            //   which prints the input String parameters
+            // - It can have any number of parameters
+
+            // Examples
+            // printer("first")
+            // printer("first", "second")
+            // printer("first", "second", "third", "fourh")
+            // ...
+            //   Console.WriteLine("What shall I print for you? Name two things!");
+            //   string textOne = Console.ReadLine();
+            // string textTwo = Console.ReadLine();
+            Printer("apple");
+            Printer("apple", "pear");
+            Printer("apple", "pear", "grapes");
+            Console.ReadLine();
+        }
+        public static void Printer(params string[] messages)
+        {
+            foreach (var message in messages) 
+            {
+                Console.Write(message + " ");
+            }
+            Console.WriteLine();
+
+        }
+        
+    }
+}
