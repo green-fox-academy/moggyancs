36e667b44fdb3f186f2f13f2b705617055e61569
moggyancs <gcsuzdi@gmail.com>
2018-02-20 16:29:13 +0100

    Lists 2-3

---
+++ week-02/day-02/Lists/02-Matchmaking/02-Matchmaking/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+    
+
+namespace _02_Matchmaking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var girls = new List<string> { "Eve", "Ashley", "Bözsi", "Kat", "Jane" };
+            var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
+
+            // Write a method that joins the two lists by matching one girl with one boy into a new list
+            // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+            Console.WriteLine(MakingMatches(girls, boys));
+            Console.ReadLine();
+        }
+
+        static string MakingMatches(List<string> girls, List<string> boys)
+        {
+            string pairs = "";
+
+            for (int i = 0; i < girls.Count; i++)
+            {
+                    pairs += girls[i] + " matched " + boys[i] + "\n";
+            }
+
+            return pairs;
+        }
+    }
+}
---
+++ week-02/day-02/Lists/03-AppendLetter/03-AppendLetter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace _03_AppendLetter
+{
+    class Program
+    {
+        private static object toArray;
+
+        static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            // Create a method called AppendA() that adds "a" to every string in the far list.
+            // The parameter should be a list.
+
+            Console.WriteLine(AppendA(far));
+            Console.ReadLine();
+            // Expected output: "kutya", "macska", "kacsa", "róka", "halacska"
+        }
+
+        static string AppendA(List<string> nimals)
+        {
+            string appended = "";
+            for (int i = 0; i < nimals.Count; i++)
+            {
+                appended += nimals[i] + "a, " ;
+            }
+            return appended;
+        }
+    }
+}
