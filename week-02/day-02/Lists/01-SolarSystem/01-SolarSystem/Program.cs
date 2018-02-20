7627f84f00f58094145a4ec1f72b746063db30d3
moggyancs <gcsuzdi@gmail.com>
2018-02-20 15:45:59 +0100

    solar system

---
+++ week-02/day-02/Lists/01-SolarSystem/01-SolarSystem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_SolarSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune" };
+           
+            // Saturn is missing from the planetList
+            // Insert it into the correct position
+            // Create a method called PutSaturn() which has list parameter and returns the correct list.
+
+            Console.WriteLine(PutSaturn(planetList));
+            // Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", ** "Uranus", "Neptune", "Saturn"
+            Console.ReadLine();
+        }
+
+        public static string PutSaturn(List<string> solar)
+        {
+            solar.Insert(5, "Saturn");
+            string fullsolar = "";
+            foreach (var item in solar)
+            {
+                fullsolar += "\"" + item + "\"" + ", ";
+            }
+            return fullsolar;
+                    
+        }
+
+    }
+}
