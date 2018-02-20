bc97a7349800645a7456bb60c7b19ed5b8d7995d
moggyancs <gcsuzdi@gmail.com>
2018-02-20 17:04:21 +0100

    Is In List & Element Finder

---
+++ week-02/day-02/Lists/05-IsInList/05-IsInList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05_IsInList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 2, 4, 6, 8, 10, 12, 14, 16 };
+            
+            // Check if list contains all of the following elements: 4,8,12,16
+            // Create a method that accepts list as an input
+            // it should return "true" if it contains all, otherwise "false"
+            Console.WriteLine(CheckNums(list));
+            Console.ReadLine();
+        }
+
+        static Boolean CheckNums (List<int> numbers)
+        {
+            Boolean valid;
+            if (numbers.Contains(4) && numbers.Contains(8) && numbers.Contains(12) && numbers.Contains(16))
+            {
+                valid = true;
+            }
+            else
+            {
+                valid = false;
+            }
+
+            return valid;
+
+        }
+    }
+}
---
+++ week-02/day-02/Lists/06-ElementFinder/06-ElementFinder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06_ElementFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var List = new List<int> { 1, 2, 3, 4, 5 };
+            Console.WriteLine(ContainsSeven(List));
+            Console.ReadLine();
+            // Write a method that checks if the arrayList contains "7" if it contains return "Hoorray" otherwise return "Noooooo"
+            // The output should be: "Noooooo"
+            // Do this again with a different solution using different list methods!
+        }
+
+        static string ContainsSeven (List<int> numbers)
+        {
+            string answer;
+            if (numbers.Contains(7))
+            {
+                answer = "Hoorray";
+            }
+            else
+            {
+                answer = "Noooooooooo";
+            }
+            return answer;
+        }
+        
+    }
+}
