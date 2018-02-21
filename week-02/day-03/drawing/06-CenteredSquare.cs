1a9e4051e57586e7179c9911eb25aa84a788d120
moggyancs <gcsuzdi@gmail.com>
2018-02-21 15:23:11 +0100

    centered square

---
+++ week-02/day-03/drawing/06-CenteredSquare.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Drawings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // Draw a green 10x10 square to the canvas' center.
+
+            foxDraw.DrawRectangle(canvas.Width / 2, canvas.Height / 2, 10, 10);
+
+        }
+
+
+
+
+    }
+}
