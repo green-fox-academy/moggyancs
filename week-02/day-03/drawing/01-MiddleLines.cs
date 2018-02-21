d8f31f736700d5d5595c6caaad5cb48ebe656e2d
moggyancs <gcsuzdi@gmail.com>
2018-02-21 10:50:14 +0100

    drawing Middle lines

---
+++ week-02/day-03/drawing/01-MiddleLines.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01_MiddleLines
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // draw a red horizontal line to the canvas' middle.
+            // draw a green vertical line to the canvas' middle.
+
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(0, canvas.Height/2, canvas.Width, canvas.Height/2);
+
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(canvas.Width/2, 0, canvas.Width/2, canvas.Height);
+    
+
+        }
+    }
+}
