e87c6b36ce011a7e9050b625aff3cbf7279a3906
moggyancs <gcsuzdi@gmail.com>
2018-02-21 13:40:07 +0100

    To the center no function

---
+++ week-02/day-03/drawing/04-ToTheCenter.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Drawings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            var FoxDraw = new foxDraw(canvas);
+            // Create a line drawing function that takes 2 parameters:
+            // The x and y coordinates of the line's starting point
+            // and draws a line from that point to the center of the canvas.
+            // Draw 3 lines with that function. Use loop for that.
+
+            double a = 0;
+            double b = 0;
+
+            for (int i = 0; i < 50; i++)
+            {
+                foxDraw.DrawLine(a, b, canvas.Width / 2, canvas.Height / 2);
+                b += 10;
+                
+            }
+           
+            
+        }
+        
+        
+    }
+}
