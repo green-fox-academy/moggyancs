71c9eaf601af578a8cb198cf546d168b88dc6a24
moggyancs <gcsuzdi@gmail.com>
2018-02-21 11:47:21 +0100

    syncheonized namespaces

--- week-02/day-03/drawing/01-MiddleLines.cs
+++ week-02/day-03/drawing/01-MiddleLines.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace _01_MiddleLines
+namespace Drawings
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
