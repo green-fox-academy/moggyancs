45a3e61392b6ea227f17eccbb3f054f07e4aa6ba
moggyancs <gcsuzdi@gmail.com>
2018-02-21 15:22:52 +0100

    horizontal lines

---
+++ week-02/day-03/drawing/05-Horizontal.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Drawings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // Create a line drawing function that takes 2 parameters:
+            // The x and y coordinates of the line's starting point
+            // and draws a 50 long horizontal line from that point.
+            // Draw 3 lines with that function. Use loop for that.
+
+            double a = 50;
+            double b = 50;
+
+            for (int i = 0; i < 5; i++)
+            {
+                foxDraw.DrawLine(a, b, a+50, b);
+                b += 10;
+
+            }
+
+
+        }
+
+
+    }
+}
