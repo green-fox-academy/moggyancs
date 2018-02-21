475f3eac6410e64e16b2aa5bc2a883a7d910eb6b
moggyancs <gcsuzdi@gmail.com>
2018-02-21 11:42:15 +0100

    diagonals

---
+++ week-02/day-03/drawing/03-Diagonals.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Drawings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new foxDraw(canvas);
+
+            // Draw the canvas' diagonals.
+            // If it starts from the upper-left corner it should be green, otherwise it should be red.
+
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(0, 0, canvas.Width, canvas.Height);
+
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine( 0, canvas.Height, canvas.Width, 0);
+
+        }
+    }
+}
