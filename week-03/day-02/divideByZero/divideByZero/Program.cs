cf601b1377fdd249017584d52d9bc6e8a3ed384a
Gerda Csuzdi <moira85@gmail.com>
2018-02-27 10:50:49 +0100

    divide by zero

---
+++ week-03/day-02/divideByZero/divideByZero/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace divideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert a number");
+            int ten = 10;
+            int number = int.Parse(Console.ReadLine());
+            try
+            {
+                double result = ten / number;
+                Console.WriteLine(result);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("You cannod divide by zero, silly");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        
+
+
+        }
+    }
+}
