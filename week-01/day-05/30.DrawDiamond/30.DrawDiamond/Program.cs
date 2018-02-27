e6fa8cd253b43ce59dfa5b005678dca3cdaf1d18
Gerda Csuzdi <moira85@gmail.com>
2018-02-27 10:52:21 +0100

    draw diamond attempt

---
+++ week-01/day-05/30.DrawDiamond/30.DrawDiamond/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _30.DrawDiamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // diamond like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //  *****
+            //   ***
+            //    *
+            //
+            // The diamond should have as many lines as the number was
+
+
+            Console.WriteLine("How big is my diamond?");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height/2; i++)
+            {
+                for (int k = height - i; k > 1; k--)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("**");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < height / 2; i++)
+            {
+                
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("**");
+                }
+                Console.WriteLine();
+
+                for (int k = height - i; k > 1; k--)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write("*");
+
+            }
+
+
+
+
+
+            Console.ReadLine();
+            {
+
+            }
+        }
+    }
+}
