aab3b41012966652f6c0e98980f5bc2dd3bfae2a
Gerda Csuzdi <moira85@gmail.com>
2018-02-27 11:30:46 +0100

    count the lines

---
+++ week-03/day-02/CountTheLines/CountTheLines/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace CountTheLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a function that takes a filename as string,
+            // then returns the number of lines the file contains.
+            // It should return zero if it can't open the file, and
+            // should not raise any error.
+
+            string filename = "myfile.txt";
+            CountTheLines(filename);
+            //CountTheLinesB(filename); - var lineCount = File.ReadLines(@"C:\file.txt").Count();
+        }
+
+        public static void CountTheLines(string path)
+        {
+
+            try
+            {
+                string[] readText = File.ReadAllLines(path);
+                Console.WriteLine(readText.Length);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine(0);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+
+        }
+    }
+}
