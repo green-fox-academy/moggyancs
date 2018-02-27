6415496bc52a688d8854ef9efd194963d406fa48
Gerda Csuzdi <moira85@gmail.com>
2018-02-27 11:50:54 +0100

    other method added

--- week-03/day-02/CountTheLines/CountTheLines/Program.cs
+++ week-03/day-02/CountTheLines/CountTheLines/Program.cs
@@ -12,9 +12,9 @@ namespace CountTheLines
             // It should return zero if it can't open the file, and
             // should not raise any error.
 
-            string filename = "myfile.txt"; //myfile.txt does not exist, my-file.txt does.
+            string filename = @"C:\Users\moggy\greenfox\moggyancs\week-03\day-02\CountTheLines\CountTheLines\my-file.txt"; //myfile.txt does not exist, my-file.txt does.
             CountTheLines(filename);
-            //CountTheLinesB(filename); - var lineCount = File.ReadLines(@"C:\file.txt").Count();
+            CountTheLinesB(filename); 
         }
 
         public static void CountTheLines(string path)
@@ -37,5 +37,24 @@ namespace CountTheLines
             
 
         }
+        public static void CountTheLinesB(string path)
+        {
+            try
+            {
+                var lineCount = File.ReadAllLines(path).Length;
+                Console.WriteLine(lineCount);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine(0);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+
     }
 }
