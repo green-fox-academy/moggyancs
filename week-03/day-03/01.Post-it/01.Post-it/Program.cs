2e667fa52300447df7685b1358ab1f5f188aaa08
Gerda Csuzdi <moira85@gmail.com>
2018-02-28 11:33:40 +0100

    Postit with constructor too

--- week-03/day-03/01.Post-it/01.Post-it/Program.cs
+++ week-03/day-03/01.Post-it/01.Post-it/Program.cs
@@ -10,26 +10,38 @@ namespace _01.Post_it
             public Color BackGroundColor;
             public string notes;
             public Color Textcolor;
+
+            public Posts (Color _BackGroundColor, string _notes, Color _TextColor)
+            {
+                BackGroundColor = _BackGroundColor;
+                notes = _notes;
+                Textcolor = _TextColor;
+
+            }
         }
   
         static void Main(string[] args)
         {
             Posts post01 = new Posts();
             post01.BackGroundColor = Color.Orange;
-            post01.notes = "Idea 1";
+            post01.notes = "Idea 1"; 
             post01.Textcolor = Color.Blue;
 
-            Posts post02 = new Posts();
-            post02.BackGroundColor = Color.Pink;
-            post02.notes = "Awesome!";
-            post02.Textcolor = Color.Black;
+            Posts post02 = new Posts(Color.Pink, "Awesome!", Color.Black);
+
+            //Posts post02 = new Posts();
+            //post02.BackGroundColor = Color.Pink;
+            //post02.notes = "Awesome!";
+            //post02.Textcolor = Color.Black;
 
             Posts post03 = new Posts();
             post03.BackGroundColor = Color.Yellow;
             post03.notes = "Superb";
             post03.Textcolor = Color.Green;
 
-
+            Console.WriteLine($"This is a note on {post01.BackGroundColor} paper, with {post01.Textcolor} letters, and says: {post01.notes}");
+            Console.WriteLine($"This is a note on {post02.BackGroundColor} paper, with {post02.Textcolor} letters, and says: {post02.notes}");
+            Console.ReadLine();
         }
     }
 }
