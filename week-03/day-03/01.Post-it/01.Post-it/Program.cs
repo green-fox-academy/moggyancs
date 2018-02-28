4c829a1bdcdb897266483a5c34e65f8fa7b6378f
Gerda Csuzdi <moira85@gmail.com>
2018-02-28 11:13:38 +0100

    postits

---
+++ week-03/day-03/01.Post-it/01.Post-it/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+
+namespace _01.Post_it
+{
+    class Program
+    {
+        public struct Posts
+        {
+            public Color BackGroundColor;
+            public string notes;
+            public Color Textcolor;
+        }
+  
+        static void Main(string[] args)
+        {
+            Posts post01 = new Posts();
+            post01.BackGroundColor = Color.Orange;
+            post01.notes = "Idea 1";
+            post01.Textcolor = Color.Blue;
+
+            Posts post02 = new Posts();
+            post02.BackGroundColor = Color.Pink;
+            post02.notes = "Awesome!";
+            post02.Textcolor = Color.Black;
+
+            Posts post03 = new Posts();
+            post03.BackGroundColor = Color.Yellow;
+            post03.notes = "Superb";
+            post03.Textcolor = Color.Green;
+
+
+        }
+    }
+}
