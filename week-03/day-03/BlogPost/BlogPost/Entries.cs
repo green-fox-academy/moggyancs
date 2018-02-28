4e1ab51f5a70e38fbe8d27c503fd580ff92537a1
Gerda Csuzdi <moira85@gmail.com>
2018-02-28 13:42:49 +0100

    Blogpost

---
+++ week-03/day-03/BlogPost/BlogPost/Entries.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogPost
+{
+    class Entries
+    {
+        public string authorName;
+        public string title;
+        public string text;
+        public string date;
+
+        public Entries(string authorName, string title, string text, string publishDate)
+        {
+            this.authorName = authorName;
+            this.title = title;
+            this.text = text;
+            this.date = publishDate;
+        }
+    }
+}
---
+++ week-03/day-03/BlogPost/BlogPost/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BlogPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Entries DJ001 = new Entries("John Doe", "Lorem Ipsum", "Lorem ipsum dolor sit amet.", "2000.05.04");
+            Entries UT001 = new Entries("Tim Urban", "Wait but why", "A popular long-form, stick-figure-illustrated blog about almost everything.", "2010.10.10.");
+            Entries TW001 = new Entries("William Turton", "One Engineer Is Trying to Get IBM to Reckon With Trump", "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.", "2017.03.28.");
+
+            Console.WriteLine("I borrowed this post from {0}, originally posted on {1}. \n {2} \n \n {3}", DJ001.authorName, DJ001.date, DJ001.title, DJ001.text);
+            Console.ReadLine();
+        }
+    }
+}
