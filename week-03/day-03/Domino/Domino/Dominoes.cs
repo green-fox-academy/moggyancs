f4e54c25073f7680beae5edcdc547c2f91b8e765
Gerda Csuzdi <moira85@gmail.com>
2018-02-28 13:43:07 +0100

    domino unfinished

---
+++ week-03/day-03/Domino/Domino/Dominoes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Domino
+{
+    public class Domino
+    {
+        private readonly int[] Values; // Values is an int type array
+
+        public Domino(int valueA, int valueB) // this is a constructor
+        {
+            this.Values = new int[] { valueA, valueB }; // creates the array of the array of numberpairs
+        }
+
+        public int[] GetValues() // allows to access the value pairs
+        {
+            return Values;
+        }
+    }
+}
