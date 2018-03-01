cdf71060ed7def0cdabbe7721b7b81dfdb0381dc
Gerda Csuzdi <moira85@gmail.com>
2018-03-01 10:58:25 +0100

    Bunnies done

---
+++ week-03/day-04/05.Bunnies/05.Bunnies/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05.Bunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We have a number of bunnies and each bunny has two big floppy ears.
+            // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+
+            int n = 7;
+            Console.WriteLine(CountBunnyEars(n));
+            Console.ReadLine();
+        }
+
+        static int CountBunnyEars(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            else
+            {
+                return  2 + CountBunnyEars(n - 1);
+            }
+        }
+
+    }
+}
