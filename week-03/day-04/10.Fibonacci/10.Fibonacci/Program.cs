6b1c528e4c27a6756adee50f3b0d5b7d59422876
Gerda Csuzdi <moira85@gmail.com>
2018-03-01 12:13:30 +0100

    fibonacci done

---
+++ week-03/day-04/10.Fibonacci/10.Fibonacci/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _10.Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The fibonacci sequence is a famous bit of mathematics, and it happens to
+            // have a recursive definition. The first two values in the sequence are
+            // 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the
+            // previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21
+            // and so on. Define a recursive fibonacci(n) method that returns the nth
+            // fibonacci number, with n=0 representing the start of the sequence.
+
+            int n = 8;
+            Console.WriteLine(Fibo(n));
+            Console.ReadLine();
+        }
+
+        static int Fibo(int n)
+        {
+            if (n <= 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibo(n - 1) + Fibo(n - 2);
+            }
+        }
+    }
+}
