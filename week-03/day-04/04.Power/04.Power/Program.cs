1b9334431ab6844909a731c13a14fce3f5eee9f2
Gerda Csuzdi <moira85@gmail.com>
2018-03-01 10:54:04 +0100

    Power done

---
+++ week-03/day-04/04.Power/04.Power/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04.Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given base and n that are both 1 or more, compute recursively (no loops)
+            // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+
+            int baseInt = 2;
+            int expInt = 10;
+
+            Console.WriteLine(Power(baseInt, expInt));
+            Console.ReadLine();
+        }
+
+        static int Power (int baseInt, int expInt)
+        {
+            if (expInt == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return baseInt * Power(baseInt, expInt - 1);
+            }
+        }
+    }
+}
