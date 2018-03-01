5174acac2304b01e7d65c9afeb3e0aa1d0f43fae
Gerda Csuzdi <moira85@gmail.com>
2018-03-01 11:46:23 +0100

    Strings again done

---
+++ week-03/day-04/08.StringsAgain/08.StringsAgain/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _07.Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively (no loops) a new string where all the
+            // lowercase 'x' chars have been changed to 'y' chars.
+
+            string myString = "x-flxboxs-x";
+            Console.WriteLine(XtoY(myString));
+            Console.ReadLine();
+
+        }
+
+        static string XtoY(string s)
+        {
+            if (s.Length == 0)
+            {
+                return s;
+            }
+            else if (s.Substring(0, 1) == "x")
+            {
+                return XtoY(s.Substring(1));
+            }
+            else
+            {
+                return s.Substring(0, 1) + XtoY(s.Substring(1));
+            }
+
+        }
+    }
+}
