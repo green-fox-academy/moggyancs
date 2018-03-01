883cbd2161406937afcd08f1d2888f6a080e6d7c
Gerda Csuzdi <moira85@gmail.com>
2018-03-01 11:53:44 +0100

    strings again and again done

---
+++ week-03/day-04/09.StringsAgainAgain/09.StringsAgainAgain/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _07.Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively a new string where all the
+            // adjacent chars are now separated by a "*".
+
+            string myString = "super";
+            Console.WriteLine(XtoY(myString));
+            Console.ReadLine();
+
+        }
+
+        static string XtoY(string s)
+        {
+            if (s.Length == 1)
+            {
+                return s;
+            }
+            else if (s.Substring(0, 1) == "*")
+            {
+                return s.Substring(0, 1) + XtoY(s.Substring(1));
+            }
+            else
+            {
+                return s.Substring(0, 1) + "*" + XtoY(s.Substring(1));
+            }
+
+        }
+    }
+}
