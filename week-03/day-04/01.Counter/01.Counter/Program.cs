2e88dd5c36be9adf363165df933b59bf5a6ce511
Gerda Csuzdi <moira85@gmail.com>
2018-03-01 10:36:15 +0100

    Counter done

---
+++ week-03/day-04/01.Counter/01.Counter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01.Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a recursive function that takes one parameter: n and counts down from n.
+            Console.WriteLine(CountDown(10));
+            Console.Read();
+        }
+
+        public static int CountDown(int n)
+        {
+            if (n == 0)
+            {
+                return n;
+            }
+            else
+            {
+                Console.WriteLine(n);
+                return CountDown( n - 1 );
+            }
+        }
+    }
+}
