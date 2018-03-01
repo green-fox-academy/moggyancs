0580e605672054b9007bff3bb0efd7b414b0c901
Gerda Csuzdi <moira85@gmail.com>
2018-03-01 12:18:40 +0100

    Refactorio

---
+++ week-03/day-04/11.Refactorio/11.Refactorio/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _11.Refactorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a recursive function called `refactorio`
+            // that returns it's input's factorial
+
+            int number = 5;
+            Console.WriteLine(Factorial(number));
+            Console.ReadLine();
+
+        }
+
+        static int Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+    }
+}
