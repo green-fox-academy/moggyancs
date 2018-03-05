26aa2bcb7212e39230f741e908a22e6654b8a29f
Gerda Csuzdi <moira85@gmail.com>
2018-03-05 01:37:00 +0100

    dice set done

--- week-03/day-03/Dice/Dice/Program.cs
+++ week-03/day-03/Dice/Dice/Program.cs
@@ -10,28 +10,28 @@ namespace Dice
         //    You can reroll with Reroll()
         //    Your task is to get where all dice is a 6
         static Random RandomValue;
-        static int[] Dices = new int[6]; // a method that creates an array of dices with 6 integers in it.
+        static int[] Dices = new int[6]; 
 
-        public int[] Roll() //this adds the first 6 integers the array above, as the results of the roll
+        public int[] Roll() 
         {
-            for (int i = 0; i < Dices.Length; i++) //end:array lenght
+            for (int i = 0; i < Dices.Length; i++) 
             {
                 Dices[i] = RandomValue.Next(1, 7);
             }
-            return Dices; //returns the dices array with 6 random elements
+            return Dices; 
         }
 
-        public int[] GetCurrent() //looks at all elements in the array
+        public int[] GetCurrent() 
         {
             return Dices;
         }
 
-        public int GetCurrent(int i) //looks at one element of the array
+        public int GetCurrent(int i) 
         {
             return Dices[i];
         }
 
-        public void Reroll() //reroll all the dices, aka reassign a value for the elements of the array
+        public void Reroll() 
         {
             for (int i = 0; i < Dices.Length; i++)
             {
@@ -39,40 +39,28 @@ namespace Dice
             }
         }
 
-        public void Reroll(int k) // reroll one dice
+        public void Reroll(int k) 
         {
             Dices[k] = new Random().Next(1, 7);
         }
 
         public static void Main(string[] args)
         {
-            RandomValue = new Random(); // goes for random
-            Dice myDice = new Dice(); //create dice class
-            myDice.GetCurrent(); // 
+            RandomValue = new Random(); 
+            Dice myDice = new Dice(); 
+            Console.WriteLine(string.Join(", ",  myDice.GetCurrent()));
             myDice.Roll();
-            myDice.GetCurrent();
-            myDice.GetCurrent(5);
-            myDice.Reroll();
-            myDice.GetCurrent();
-            myDice.Reroll(4);
-            myDice.GetCurrent();
-
-              for (int i = 0; i < myDice.GetCurrent().Length; i++)
+            Console.WriteLine(string.Join(", ", myDice.GetCurrent()));
+            
+            for (int i = 0; i < myDice.GetCurrent().Length; i++)
             {
-                    if (myDice.GetCurrent(i) != 6)
-                    {
-                        Console.Write(myDice.GetCurrent(i) + ", ");
-                        myDice.Reroll(i);
-                    }
-
-                    else
-                    {
-                        Console.WriteLine(myDice.GetCurrent(i));
-                    }
+                while ((myDice.GetCurrent(i) != 6))
+                {
+                 myDice.Reroll(i);
+                }
+                    Console.WriteLine(string.Join(", ", myDice.GetCurrent()));      
             }
             Console.ReadLine();
-
-
         }
     }
 }
