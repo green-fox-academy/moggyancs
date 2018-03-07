f8c030eade9d79806d0bd84ab510e53df821c76c
Gerda Csuzdi <moira85@gmail.com>
2018-03-07 14:33:52 +0100

    interface inserted

---
+++ week-04/day-03/w04d03e02Comparator/w04d03e02Comparator/Dominoes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace w04d03e02Comparator
+{
+    public class Domino : IComparable<Domino>
+    {
+        private readonly int[] Values; 
+
+        public Domino(int valueA, int valueB) 
+        {
+            this.Values = new int[] { valueA, valueB }; 
+        }
+
+        public int CompareTo(Domino other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[] GetValues() 
+        {
+            return Values;
+        }
+    }
+}
---
+++ week-04/day-03/w04d03e02Comparator/w04d03e02Comparator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace w04d03e02Comparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Domino> dominoes = new List<Domino>();
+            dominoes.Add(new Domino(5, 2));
+            dominoes.Add(new Domino(4, 6));
+            dominoes.Add(new Domino(1, 5));
+            dominoes.Add(new Domino(6, 7));
+            dominoes.Add(new Domino(2, 4));
+            dominoes.Add(new Domino(7, 1));
+
+            //var sorted = new List<Domino>();
+
+            dominoes.Sort();
+
+            Console.WriteLine(string.Join(" ", dominoes));
+            Console.ReadLine();
+        }
+    }
+}
