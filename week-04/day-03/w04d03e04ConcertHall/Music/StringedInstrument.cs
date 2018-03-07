2f5b4c95c6f4cdc5dbd983e0dee5fb914f0ad078
Gerda Csuzdi <moira85@gmail.com>
2018-03-07 17:24:18 +0100

    Concert hall first classes

---
+++ week-04/day-03/w04d03e04ConcertHall/Music/StringedInstrument.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Music
+{
+    abstract class StringedInstrument : Instrument
+    {
+        private int numberOfStrings;
+        abstract public string Sound();
+
+        public void Instrument()
+        {
+
+        }
+
+        public override void Play()
+        {
+            Console.WriteLine();
+        }
+
+
+    }
+}
---
+++ week-04/day-03/w04d03e04ConcertHall/w04d03e04ConcertHall/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace w04d03e04ConcertHall
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test 1, create Electric Guitar, Bass Guitar and Violin with default strings.");
+            //var guitar = new ElectricGuitar();
+            //var bassGuitar = new BassGuitar();
+            //var violin = new Violin();
+
+            Console.WriteLine("Test 1 Play");
+            //guitar.Play();
+            //bassGuitar.Play();
+            //violin.Play();
+
+            Console.WriteLine("Test 2, create Electric Guitar, Bass Guitar with 7 and 5 strings .");
+            //ElectricGuitar guitar2 = new ElectricGuitar(7);
+            //BassGuitar bassGuitar2 = new BassGuitar(5);
+
+            Console.WriteLine("Test 2 Play");
+            //guitar2.Play();
+            //bassGuitar2.Play();
+
+            Console.ReadLine();
+        }
+
+    }
+
+}
