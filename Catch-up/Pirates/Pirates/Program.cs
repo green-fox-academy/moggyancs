0ae2545a7b929a9984bffd97d0dac410f4f38eb6
Gerda Csuzdi <moira85@gmail.com>
2018-03-13 12:55:44 +0100

    Pirates - pirates & Ship class

---
+++ Catch-up/Pirates/Pirates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Pirates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pirate john = new Pirate("John Smith");
+            Pirate hook = new Pirate("Captain James Hook");
+            Pirate beard = new Pirate("BlackBeard");
+            Pirate roberts = new Pirate("Bartholomew Roberts");
+            Pirate morgan = new Pirate("Captain Henry Morgan");
+            Pirate bonny = new Pirate("Anne Bonny");
+            Pirate read = new Pirate("Mary Read");
+            Pirate sparrow = new Pirate("Captain Jack Sparrow");
+            Pirate smee = new Pirate("Mr. Smee");
+
+            Ship pearl = new Ship("The Black Pearl");
+            Ship roger = new Ship("The Jolly Roger");
+            Ship jamaica = new Ship("Jamaica");
+
+            jamaica.FillShip(morgan);
+        }
+    }
+}
---
+++ Catch-up/Pirates/Pirates/Ship.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pirates
+{
+    class Ship
+    {
+        private string name;
+        private Pirate captain;
+        private List<Pirate> crew;
+
+        public Ship(string name)
+        {
+            this.name = name;
+        }
+
+        public void FillShip(Pirate captain)
+        {
+            this.captain = captain;  
+
+        }
+        public void GetMyCrew()
+        {
+            Console.WriteLine($"This is the {name}, whose captain is the feared {captain}, served by ");
+            foreach (var member in crew)
+            {
+            Console.WriteLine(member + " - " + (member.IsAwake() ? "ready to serve" : "passed out"));
+            }
+        }
+
+    
+    }
+
+
+}
