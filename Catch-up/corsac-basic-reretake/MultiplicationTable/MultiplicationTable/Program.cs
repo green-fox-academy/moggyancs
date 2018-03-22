4a4289321536c29c18d67512ffc2bb6c8740642a
Gerda Csuzdi <moira85@gmail.com>
2018-03-22 15:22:15 +0100

    method returns string

--- Catch-up/corsac-basic-reretake/MultiplicationTable/MultiplicationTable/Program.cs
+++ Catch-up/corsac-basic-reretake/MultiplicationTable/MultiplicationTable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace MultiplicationTable
 {
@@ -6,27 +7,21 @@ namespace MultiplicationTable
     {
         static void Main(string[] args)
         {
-            // Create a function that prints the multiplication table of a given base number.
-            // It should take the base number as a parameter, and prints the output to the
-            // console.
-
-            // The output should look like this for 5 as base:
-            //  1 * 5 = 5
-
             Console.WriteLine("Please add base number: ");
             int baseNumber = int.Parse(Console.ReadLine());
-            Multiply(baseNumber);
+            Console.WriteLine(Multiply(baseNumber));
 
             Console.ReadLine();
         }
 
-        static void Multiply(int baseNumber)
+        static string Multiply(int baseNumber)
         {
-            Console.WriteLine($"The multiplication table for {baseNumber} is the following:");
+            string table = $"The multiplication table for {baseNumber} is the following:";
             for (int i = 1; i <= 10; i++)
             {
-                Console.WriteLine($"{i} * {baseNumber} = {i * baseNumber}");
+                table += $"{i} * {baseNumber} = {i * baseNumber} \n";
             }
+            return table;
         }
     }
 }
