5102503462e6b516f8a2a0be866762e9ecc9508d
Gerda Csuzdi <moira85@gmail.com>
2018-03-22 15:53:06 +0100

    slpit methods

--- Catch-up/corsac-basic-reretake/crashes/Crashes/Crashes/Program.cs
+++ Catch-up/corsac-basic-reretake/crashes/Crashes/Crashes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Crashes
@@ -7,12 +8,62 @@ namespace Crashes
     {
         static void Main(string[] args)
         {
-            string path = "../../../../../crash-incidents.csv";
+            string path = "./crash-incidents.csv";
             WeatherCircumstance(path);
-                
+
+            //CA solution
+            string[] lines = ReadLines(path);
+            var weatherconditions = ParseWeatherConditions(lines);
             Console.ReadLine();
         }
 
+        static string[] ReadLines(string path)
+        {
+            return File.ReadAllLines("./crash-incidents.csv");
+        }
+
+        static List<string> ParseWeatherConditions(string[] rawCrashIncidents)
+        {
+            List<string> weatherConditions = new List<string>();
+            foreach (var line in rawCrashIncidents)
+            {
+                weatherConditions.Add(line.Split(';')[5]);
+            }
+            return weatherConditions;
+        }
+
+        public void CountAccidents(List<string> weatherCondition)
+        {
+            var badConditions = new List<string>
+            {
+                "RAIN", "FREEZING RAIN", "SNOW", "FOG", "SEVERE CROSSWINDS"
+            };
+
+            var goodWeather = new List<string>
+            {
+                "CLOUDY", "CLEAR"
+            };
+
+            int badConditionCount = 0;
+            int goodConditionCount = 0;
+
+            foreach (var condition in weatherCondition)
+            {
+                if (badConditions.Contains(condition))
+                {
+                    badConditionCount++;
+                }
+
+                else if (goodWeather.Contains(condition))
+                {
+                    goodConditionCount++;
+                }
+            }
+            Console.WriteLine($"CA The amount of crashes at good weather conditions: {goodConditionCount}");
+            Console.WriteLine($"CA The amount of crashes at bad weather conditions: {badConditionCount}");
+        }
+
+        // my solution
         static void WeatherCircumstance(string path)
         {
             string[] lines = File.ReadAllLines(path);
