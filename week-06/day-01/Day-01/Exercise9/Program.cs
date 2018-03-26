162694df2df1284d6cecf41c4b29044ed9559a0c
Gerda Csuzdi <moira85@gmail.com>
2018-03-26 16:17:01 +0200

    add ex.9 solution

---
+++ week-06/day-01/Day-01/Exercise9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to convert a char array to a string!
+
+            char[] characters = { 'a', ' ', 's', 't', 'r', 'i', 'n', 'g' };
+
+            string s = string.Join("", characters);
+
+            var CharsToStringMethod = String.Concat(characters);
+
+            Console.WriteLine(s);
+            Console.WriteLine(CharsToStringMethod);
+            Console.ReadLine();
+        }
+    }
+}
