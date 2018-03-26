1b191bc31a362caf48396dad60f407c331d56fce
Gerda Csuzdi <moira85@gmail.com>
2018-03-26 15:27:53 +0200

    add uppercase couinter

---
+++ week-06/day-01/Day-01/Exercise08/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the uppercase characters in a string!
+
+            string target = "theQuickBrownFoxJumpsOverTheLazyDog";
+
+            var UpperCaseQuery = from chars in target
+                                 where chars.Equals(Char.ToUpper(chars))
+                                 group chars by chars;
+
+            var UpperCaseMethod = target.
+                                 Where(c => Char.IsUpper(c)).
+                                 GroupBy(c => c);
+
+            Console.WriteLine(UpperCaseQuery.Count());
+            Console.WriteLine(UpperCaseMethod.Count());
+
+            Console.ReadLine();
+        }
+    }
+}
