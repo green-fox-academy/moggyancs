ed6bb750177c2295a44b0241afb43c6ce9062c72
Gerda Csuzdi <moira85@gmail.com>
2018-03-26 14:16:06 +0200

    add string search

---
+++ week-06/day-01/Day-01/Exercise07/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the strings which starts with A and ends with I in the following array:
+
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            var stringSearchQuery = from words in cities
+                               where words.StartsWith("A") && words.EndsWith("I")
+                               select words;
+
+            foreach (var word in stringSearchQuery)
+            {
+                Console.WriteLine(word);
+            }
+
+            var stringSearchMethod = cities.Where(word => word.StartsWith("A") && word.EndsWith("I"));
+
+            foreach (var word in stringSearchMethod)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
