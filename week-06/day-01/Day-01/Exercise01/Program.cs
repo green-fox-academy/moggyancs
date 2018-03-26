2a9313c890b6f52342c1475788f12b8250c576f5
Gerda Csuzdi <moira85@gmail.com>
2018-03-26 11:34:00 +0200

    feat print even numbers

---
+++ week-06/day-01/Day-01/Exercise01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to get the even numbers from the following array:
+
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var evenNumbersFromQuery = from num in n
+                                       where num % 2 == 0
+                                       select num;
+
+            foreach (var number in evenNumbersFromQuery)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+
+            var evenNumbersFromMethod = n
+                                        .Where(number => number % 2 == 0)
+                                        .Select(number => number);
+
+            foreach (var number in evenNumbersFromMethod)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
