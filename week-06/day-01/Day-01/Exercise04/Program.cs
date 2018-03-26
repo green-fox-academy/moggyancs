f40dbeebb791a1399ec9dfc10f0f547d3d256b0f
Gerda Csuzdi <moira85@gmail.com>
2018-03-26 12:03:52 +0200

    add big Square numbers

---
+++ week-06/day-01/Day-01/Exercise04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find which number squared value is more then 20 from the following array:
+
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+            var bigSquareQuary = from nums in n
+                                 where nums * nums > 20
+                                 select nums;
+
+            foreach (var number in bigSquareQuary)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+            var bigSquareMethod = n.Where(number => number * number > 20).Select(number => number);
+
+            foreach (var number in bigSquareMethod)
+            {
+                Console.Write(number + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
