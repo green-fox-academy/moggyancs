7850fefb7e99acde6dc306ad4581d9908c941d7d
Gerda Csuzdi <moira85@gmail.com>
2018-03-26 13:28:13 +0200

    add frequency count

---
+++ week-06/day-01/Day-01/Exercise05/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the frequency of numbers in the following array:
+
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var FrequencyQuery = from nums in n
+                                 orderby nums
+                                 group nums by nums;
+
+            foreach (var frNo in FrequencyQuery)
+            {
+                Console.WriteLine($"Number {frNo.Key} appears {frNo.Count()} times.");
+            }
+
+            Console.WriteLine();
+            var FrequencyMethod = n.GroupBy(number => number).OrderBy(number => number.Key);
+
+            foreach (var number in FrequencyMethod)
+            {
+                Console.WriteLine($"Number {number.Key} appears {number.Count()} times.");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
