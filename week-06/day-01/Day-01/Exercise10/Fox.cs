fb8855fed2141b0f3b946191fea36d390b29148a
Gerda Csuzdi <moira85@gmail.com>
2018-03-26 16:17:19 +0200

    add ex.10 solution

---
+++ week-06/day-01/Day-01/Exercise10/Fox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise10
+{
+    class Fox
+    {
+        Random rnd = new Random();
+        private string name;
+        private string type;
+        private string color;
+
+        public Fox(string name, string type, string color)
+        {
+            this.Name = name;
+            this.Color = color;
+            this.Type = type;
+        }
+
+        public string Type { get => type; set => type = value; }
+        public string Color { get => color; set => color = value; }
+        public string Name { get => name; set => name = value; }
+    }
+}
---
+++ week-06/day-01/Day-01/Exercise10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a Fox class with 3 properties(name, type, color) Fill a list with at least 5 foxes, 
+            //it's up to you how you name/create them! Write a LINQ Expression to find the foxes with green color! 
+            //Write a LINQ Expression to find the foxes with green color and pallida type!
+
+            Fox one = new Fox("Nick Wilde", "macrotis", "green");
+            Fox two = new Fox("Robin Hood", "pallida", "grey");
+            Fox three = new Fox("Eevee", "corsac", "green");
+            Fox four = new Fox("ninetails", "pallida", "green");
+            Fox five = new Fox("swiper", "totoro", "white");
+
+            List<Fox> cohort = new List<Fox> { one, two, three, four, five };
+
+            var GreenFoxes = cohort.Where(fox => fox.Color.Equals("green"));
+            var GreenAndPallida = GreenFoxes.Where(fox => fox.Type.Equals("pallida"));
+
+            foreach (var fox in GreenFoxes)
+            {
+                Console.WriteLine($"{fox.Name} is a {fox.Color} {fox.Type} type fox");
+            }
+
+            Console.WriteLine();
+
+            foreach (var fox in GreenAndPallida)
+            {
+                Console.WriteLine($"{fox.Name} is a {fox.Color} {fox.Type} type fox");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
