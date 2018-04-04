d03a7ce797e342c6aa525acc87e258cfe251e5af
Gerda Csuzdi <moira85@gmail.com>
2018-04-04 15:43:17 +0200

    part I done, string format done

---
+++ week-07/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using BankOfSimba.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BankOfSimba.Controllers
+{
+    [Route("simba")]
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+
+
+        public IActionResult Index()
+        {
+            BankAccount simba = new BankAccount("Simba", "2000", "Lion");
+            return View(simba);
+        }
+    }
+}
---
+++ week-07/BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -0,0 +1,17 @@
+﻿namespace BankOfSimba.Models
+{
+    public class BankAccount
+    {
+        public string Name { get; set; }
+        public string Balance { get; set; }
+        public string AnimalType { get; set; }
+
+        public BankAccount(string name, string balance, string animalType)
+        {
+            Name = name;
+            Balance = balance;
+            AnimalType = animalType;
+        }
+
+    }
+}
