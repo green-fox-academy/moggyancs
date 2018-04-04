e52db54f13a52d3ef40395ae73dcb0e2922e8887
Gerda Csuzdi <moira85@gmail.com>
2018-04-04 17:52:57 +0200

    minor changes in style

--- week-07/BankOfSimba/BankOfSimba/Models/BankAccount.cs
+++ week-07/BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -7,14 +7,16 @@
         public string AnimalType { get; set; }
         public int ID { get; set; }
         private static int totalID;
+        public bool Royalty { get; set; }
 
-        public BankAccount(string name, string balance, string animalType)
+        public BankAccount(string name, string balance, string animalType, bool royalty)
         {
             ID = totalID;
             Name = name;
             Balance = balance;
             AnimalType = animalType;
             totalID++;
+            Royalty = royalty;
         }
 
     }
--- week-07/BankOfSimba/BankOfSimba/Models/BankAccounts.cs
+++ week-07/BankOfSimba/BankOfSimba/Models/BankAccounts.cs
@@ -6,16 +6,16 @@ namespace BankOfSimba.Models
     {
         List<BankAccount> Accounts = new List<BankAccount>()
         {
-            new BankAccount("Simba", "2000", "East African Lion"),
-            new BankAccount("Nala", "5637.4", "East African Lion"),
-            new BankAccount("Timon", "2", "Meerkat"),
-            new BankAccount("Pumbaa", "20", "Common Warthog"),
-            new BankAccount("Scar", "500", "East African Lion"),
-            new BankAccount("Zazu", "1200.43", "Tanzanian Red-billed Hornbill"),
-            new BankAccount("Rafiki", "5463", "Mandrill"),
-            new BankAccount("Shenzi", "45254", "Spotted Hyena"),
-            new BankAccount("Banzai", "24525", "Spotted Hyena"),
-            new BankAccount("Ed", "4525", "Spotted Hyena")
+            new BankAccount("Simba", "2000", "East African Lion", true),
+            new BankAccount("Nala", "5637.4", "East African Lion", true),
+            new BankAccount("Timon", "2", "Meerkat", false),
+            new BankAccount("Pumbaa", "20", "Common Warthog", false),
+            new BankAccount("Scar", "500", "East African Lion", true),
+            new BankAccount("Zazu", "1200.43", "Tanzanian Red-billed Hornbill", false),
+            new BankAccount("Rafiki", "5463", "Mandrill", false),
+            new BankAccount("Shenzi", "45254", "Spotted Hyena", false),
+            new BankAccount("Banzai", "24525", "Spotted Hyena", false),
+            new BankAccount("Ed", "4525", "Spotted Hyena", false)
 
         };
 
