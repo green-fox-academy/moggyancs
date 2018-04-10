9eeadccea20a1264d016879b9e58fef261cb41b7
Gerda Csuzdi <moira85@gmail.com>
2018-04-10 11:12:34 +0200

    set up db, feat todo class

---
+++ week-08/day-02/ToDoApp/ToDoApp/Models/ToDo.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoApp.Models
+{
+    public class ToDo
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public bool IsUrgent { get; set; }
+        public bool IsDone { get; set; }
+
+
+    }
+}
---
+++ week-08/day-02/ToDoApp/ToDoApp/ToDoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApp.Models;
+
+namespace ToDoApp
+{
+    public class ToDoContext : DbContext
+    {
+        public DbSet<ToDo> ToDos { get; set; }
+        public ToDoContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
