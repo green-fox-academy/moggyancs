b086b30d17d37df461f8ac9bda93f4def9f75dc2
Gerda Csuzdi <moira85@gmail.com>
2018-04-19 16:07:00 +0200

    Initialize project, add DB

---
+++ Catch-up/SpaceTravelling/SpaceTravelling/Models/SpaceXContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SpaceTravelling.Models
+{
+    public partial class SpaceXContext : DbContext
+    {
+        public virtual DbSet<Planet> Planet { get; set; }
+        public virtual DbSet<Spaceship> Spaceship { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SpaceX;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Planet>(entity =>
+            {
+                entity.ToTable("planet");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Population).HasColumnName("population");
+            });
+
+            modelBuilder.Entity<Spaceship>(entity =>
+            {
+                entity.ToTable("spaceship");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.MaxCapacity).HasColumnName("max_capacity");
+
+                entity.Property(e => e.Planet)
+                    .HasColumnName("planet")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Utilization).HasColumnName("utilization");
+            });
+        }
+    }
+}
--- Catch-up/SpaceTravelling/SpaceTravelling/Startup.cs
+++ Catch-up/SpaceTravelling/SpaceTravelling/Startup.cs
@@ -1,29 +1,29 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using SpaceTravelling.Entities;
-using System;
 
 namespace SpaceTravelling
 {
     public class Startup
     {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-            services.AddDbContext<SpaceXContext>(options => options.UseSqlServer(Environment.GetEnvironmentVariable("SQL_CONNECTIONSTRING")));
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseMvc();
 
             app.Run(async (context) =>
             {
