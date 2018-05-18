f8b369088bb67e1c663ebbf15aec4090117b5625
Gerda Csuzdi <moira85@gmail.com>
2018-05-18 10:22:17 +0200

    init (ShinyStars) add full project

---
+++ Catch-up/ShinyStars/ShinyStars/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ShinyStars
+{
+    public partial class MainWindow : Window
+    {
+        FoxDraw foxDraw;
+        Random random;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            foxDraw = new FoxDraw(canvas);
+            random = new Random();
+        }
+
+        public void DrawManyRandomStars(int numberOfStars)
+        {
+            foxDraw.BackgroundColor(Colors.Black);
+            foxDraw.StrokeColor(Colors.Black);
+
+            for (int i = 0; i < numberOfStars; i++)
+            {
+                byte highColor = (byte)random.Next(150, 255);
+                byte lowColor = (byte)random.Next(0, 150);
+
+                int randomSize = random.Next(2, 15);
+
+                foxDraw.FillColor(Color.FromRgb(lowColor, highColor, lowColor));
+                foxDraw.DrawRectangle(random.Next((int)canvas.Width - randomSize),
+                    random.Next((int)canvas.Height - randomSize), randomSize, randomSize);
+            }
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            canvas.Children.Clear();
+            int randomStars = random.Next(100, 300);
+            DrawManyRandomStars(randomStars);
+        }
+    }
+}
