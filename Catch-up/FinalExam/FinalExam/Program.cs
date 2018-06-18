c6cee33bdc242f83e753547a17dcc0266e60e9b3
Gerda Csuzdi <moira85@gmail.com>
2018-06-18 10:00:22 +0200

    add (exam) reverse string method

---
+++ Catch-up/FinalExam/FinalExam/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FinalExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please add a string to reverse!");
+
+            string input = Console.ReadLine();
+            string output = ReverseThis(input);
+
+            Console.WriteLine("Read from the end: " + output);
+            Console.ReadLine();
+        }
+
+        private static string ReverseThis(string toReverse)
+        {
+            string reversed = String.Empty;
+            foreach (char ch in toReverse)
+            {
+                reversed = ch + reversed;
+            }
+
+            return reversed;
+        }
+    }
+}
