d041a94d3221fc3da1076b6a7d71f4899b1dee21
Gerda Csuzdi <moira85@gmail.com>
2018-06-18 10:01:30 +0200

    Anagram test, apple test, sumtest

---
+++ Catch-up/Testing/TestingUnitTests/BasicTests/AnagramTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using Testing.Models;
+
+namespace TestingUnitTests.BasicTests
+{
+    [TestFixture]
+    public class AnagramTests
+    {
+        [TestCase("a", "a")]
+        [TestCase("ab", "ba")]
+        [TestCase("dog", "god")]
+        [TestCase("abc123", "a1b2c3")]
+        [TestCase("coins kept", "in pockets")]
+        public void RealAnagramsReturnTrue(string first, string second)
+        {
+            Assert.IsTrue(Anagram.IsAnagram(first, second));
+        }
+
+        [TestCase]
+        public void UnequalLengthWordsReturnFalse()
+        {
+            Assert.IsFalse(Anagram.IsAnagram("good", "god"));
+        }
+
+        [TestCase ("batman", null)]
+        [TestCase(null, null)]
+        [TestCase(null, "BATMAN")]
+        public void MissingArgument_TrhowsException(string first, string second)
+        {
+            Assert.Throws<NullReferenceException>(() => Anagram.IsAnagram(first, second));
+        }
+    }
+}
